b3be2c9bd32c961932872d6c986e1671cf411df2
Felipe Vaz Peres <felipe.vzps@gmail.com>
2023-12-15 16:46:50 -0300

    Update autofishing.py

--- autofishing.py
+++ autofishing.py
@@ -2,7 +2,7 @@ import pyautogui
 import keyboard
 from time import sleep
 
-loop_ativo = False  # Inicialmente, o loop está pausado
+loop_ativo = False  # Inittialy, the loop is off
 
 def iniciar_loop():
     global loop_ativo
@@ -12,7 +12,7 @@ def pausar_loop():
     global loop_ativo
     loop_ativo = False
 
-# Registre as teclas de ativação e pausa
+# Register keys to turn on/off the auto fishing
 keyboard.add_hotkey('p', iniciar_loop)
 keyboard.add_hotkey('k', pausar_loop)
 
