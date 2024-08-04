a459e5ba880f45013480573694fc4a75650f6af7
Felipe Vaz Peres <felipe.vzps@gmail.com>
2024-08-03 22:25:26 -0300

    working on multiple servers, functions documented with docstrings

--- cavebot-fiesta/functions.py
+++ cavebot-fiesta/functions.py
@@ -1,6 +1,7 @@
 import pyautogui
 import keyboard
-import config
+#import config_nostalther as config       # import nostalther config
+import config_antiga as config            # import antiga config
 import threading
 from collections import defaultdict
 from time import sleep
@@ -8,54 +9,45 @@ from time import sleep
 loop_status = False
 
 def start_loop():
-  global loop_status
-  loop_status = True
+    """
+    Start the loop by setting the global loop_status to True.
+    """
+    global loop_status
+    loop_status = True
 
 def stop_loop():
-  global loop_status
-  loop_status = False
+    """
+    Stop the loop by setting the global loop_status to False.
+    """
+    global loop_status
+    loop_status = False
 
 # register start/stop keys
 keyboard.add_hotkey('page up', start_loop)
 keyboard.add_hotkey('page down', stop_loop)
 
-# conjure rune (cast spell saved on F12 hotkey)
 def conjure_rune():
-  mana = pyautogui.locateOnScreen(config.img_dir + "mana" + ".PNG", confidence=0.7, region=config.REGION_MANA)
-  if mana != None:
-    keyboard.press_and_release('F1')
+    """
+    Conjure a rune if mana is available. It locates the mana on the screen
+    and uses the F1 hotkey to cast the spell.
+    """
+    mana = pyautogui.locateOnScreen(config.img_dir + "mana" + ".PNG", confidence=0.7, region=config.REGION_MANA)
+    if mana is not None:
+        keyboard.press_and_release('F1')
 
-# eat food on REGION_ARROW slot
 def eat_food():
-  pyautogui.moveTo(config.REGION_ARROW)
-  for i in range(2):
-    pyautogui.click(config.REGION_ARROW, button='right')
+    """
+    Eat food from the REGION_ARROW slot by right-clicking it twice.
+    """
+    pyautogui.moveTo(config.REGION_ARROW)
+    for i in range(2):
+        pyautogui.click(config.REGION_ARROW, button='right')
 
-'''
-# search for monster and attack if it is on battle
-def attack_next_monster(): 
-  monster_on_battle = True
-  monster_attacked = False
-  while monster_on_battle != None:
-    for target in config.target_list:
-      targeting = pyautogui.locateOnScreen(config.img_dir + "targeting_" + target + ".PNG", confidence=0.99, region=config.REGION_BATTLE)
-      monster_on_battle = pyautogui.locateOnScreen(config.img_dir + target + ".PNG", confidence=0.9, region=config.REGION_BATTLE)
-      if monster_on_battle and not targeting:
-        sleep(1)
-        pyautogui.moveTo(config.MONSTER_IN_BATTLE)
-        sleep(0.5)
-        pyautogui.click(button="left")
-        monster_attacked = True
-        break
-    else:
-      # If no monster is found in the loop, exit the while loop
-      break
-  if monster_attacked:
-     open_corpse()
-'''
-
-# search for monster and attack if it is on battle
 def attack_next_monster():
+    """
+    Search for a monster and attack it if it is found in the battle region.
+    If the battle is clean and a monster was attacked, it will open the dead corpses.
+    """
     monster_attacked = False
     while True:
         monster_on_battle = False
@@ -77,17 +69,21 @@ def attack_next_monster():
     if monster_attacked:
         open_corpse()
 
-# open dead corpse in REGION_PLAYER (8x8 sqm)
 def open_corpse():
-  pos_list = [(1223, 206), (1263, 208), (1301, 213), (1305, 252), (1302, 289), (1265, 285), (1226, 287), (1220, 251)]
-  for pos in pos_list:
-    pyautogui.moveTo(pos[0], pos[1])
-    pyautogui.click(button="right")
-    #sleep(0.3)
-    
+    """
+    Open the dead corpses in the REGION_PLAYER (8x8 sqm). It moves the mouse to pre-defined
+    positions and right-clicks to open corpses.
+    """
+    for pos in config.POS_LIST:
+        pyautogui.moveTo(pos[0], pos[1])
+        pyautogui.click(button="right")
+
+
 '''
-# open dead corpse in REGION_PLAYER (8x8 sqm)
+# TODO: The corpses of some creatures are not being found. 
+# find a more effective method to locate them.
 def open_corpse(target_monster):
+  # find and open dead corpse in REGION_PLAYER (8x8 sqm)
   count = len(target_monster)
   for monster in range(0,(count)):
     dead_monster = pyautogui.locateAllOnScreen(config.img_dir + target_monster[monster] + ".PNG", confidence=0.96, region=config.REGION_PLAYER)
@@ -99,34 +95,53 @@ def open_corpse(target_monster):
       sleep(0.8)
 '''
 
-# eat food from corpse loot (meat, ham, etc)
 def eat_food_from_corpse(fooditems):
-   count = len(fooditems)
-   for item in range(0,(count)):
-      food = pyautogui.locateAllOnScreen(config.img_dir + fooditems[item] + ".PNG", confidence=0.9, region=config.REGION_LOOT)
-      for item in food:
-         center_x, center_y = pyautogui.center(item)
-         pyautogui.moveTo(center_x, center_y)
-         sleep(0.5)
-         pyautogui.click(button="right") 
+    """
+    Eat food from the corpse loot by right-clicking on identified food items in the REGION_LOOT.
+    
+    Parameters:
+    fooditems (list): List of food item image names to locate and eat.
+    """
+    count = len(fooditems)
+    for item in range(count):
+        food = pyautogui.locateAllOnScreen(config.img_dir + fooditems[item] + ".PNG", confidence=0.9, region=config.REGION_LOOT)
+        for item in food:
+            center_x, center_y = pyautogui.center(item)
+            pyautogui.moveTo(center_x, center_y)
+            sleep(0.5)
+            pyautogui.click(button="right") 
 
-# loot items from corpse
 def loot_corpse(lootitems):
-  backpack = pyautogui.locateOnScreen(config.bpname)
-  count = len(lootitems)
-  for item in range(0,(count)):
-    gold = pyautogui.locateAllOnScreen(config.img_dir + lootitems[item] + ".PNG", confidence=0.9, region=config.REGION_LOOT)
-    for item in gold:
-      center_x, center_y = pyautogui.center(item)
-      pyautogui.moveTo(center_x, center_y)
-      sleep(0.5)
-      pyautogui.dragTo(backpack.left, backpack.top+20, duration=0.2)
-      sleep(0.5)
-      pyautogui.press('enter')
-      sleep(1)
+    """
+    Loot items from a corpse by dragging them to the backpack.
+    
+    Parameters:
+    lootitems (list): List of loot item image names to locate and loot.
+    """
+    backpack = pyautogui.locateOnScreen(config.bpname)
+    count = len(lootitems)
+    for item in range(count):
+        gold = pyautogui.locateAllOnScreen(config.img_dir + lootitems[item] + ".PNG", confidence=0.9, region=config.REGION_LOOT)
+        for item in gold:
+            center_x, center_y = pyautogui.center(item)
+            pyautogui.moveTo(center_x, center_y)
+            sleep(0.5)
+            pyautogui.dragTo(backpack.left, backpack.top + 20, duration=0.2)
+            sleep(0.5)
+            pyautogui.press('enter')
+            sleep(1)
 
-# find goldcoin group position (cluster)
 def find_coin_positions(coin_image, region):
+    """
+    Find gold coin positions (clusters) on the screen within a specified region.
+    
+    Parameters:
+    coin_image (str): The image file name of the coin.
+    region (tuple): The region of the screen to search for coins.
+    
+    Returns:
+    dict: A dictionary of coin positions grouped by their proximity.
+    """
     coin_positions = defaultdict(list)
     find_objects = pyautogui.locateAllOnScreen(coin_image, confidence=0.9, region=region)
 
@@ -146,8 +161,13 @@ def find_coin_positions(coin_image, region):
 
     return coin_positions
 
-# loot goldcoins from corpse
 def loot_goldcoin(coins):
+    """
+    Loot gold coins from a corpse by dragging them to the backpack.
+    
+    Parameters:
+    coins (list): List of coin image names to locate and loot.
+    """
     backpack = pyautogui.locateOnScreen(config.bpname)
     if not backpack:
         print("Backpack not found")
@@ -169,18 +189,24 @@ def loot_goldcoin(coins):
                     center_y = position[1] + position[3] // 2
                     pyautogui.moveTo(center_x, center_y)
                     sleep(0.5)
-                    pyautogui.dragTo(backpack.left, backpack.top + 20, duration=0.2)
+                    pyautogui.dragTo(backpack.left, backpack.top + 20, duration=0.3)
                     sleep(0.1)
                     pyautogui.press('enter')
-                    sleep(0.3) # 1
+                    sleep(0.3) 
                     looted = True
                     break  # break after looting the first coin in the group
 
             if not looted:
                 break  # exit if no coin was looted in this iteration
 
-# drop loot on the floor
 def drop_loot_on_floor(dropitems, bags):
+    """
+    Drop loot on the floor by dragging items from bags to the player's location.
+    
+    Parameters:
+    dropitems (list): List of items to drop.
+    bags (list): List of bag image names to locate and open.
+    """
     for bag in bags:
         bag_positions = list(pyautogui.locateAllOnScreen(config.img_dir + bag + ".PNG", confidence=0.99, region=config.REGION_LOOT))
         for position in bag_positions:
@@ -199,24 +225,36 @@ def drop_loot_on_floor(dropitems, bags):
             pyautogui.dragTo(config.PLAYER_SQM, duration=0.8)
             sleep(0.5)
 
-# move mouse to center of the image
 def move(location):
-  x,y = pyautogui.center(location)
-  pyautogui.moveTo(x, y)
+    """
+    Move the mouse to the center of the specified location.
+    
+    Parameters:
+    location (tuple): The location to move the mouse to.
+    """
+    x, y = pyautogui.center(location)
+    pyautogui.moveTo(x, y)
 
-# click on minimap
 def move_and_click(location):
-  move(location)
-  pyautogui.click()
-  sleep(0.5)
+    """
+    Move the mouse to the center of the specified location and click.
+    
+    Parameters:
+    location (tuple): The location to move the mouse to and click.
+    """
+    move(location)
+    pyautogui.click()
+    sleep(0.5)
 
-# function that open a thread for attack_next_rotworm()
 def thread_attack_monster():
-    while True:  # infinite loop to continuos attack rotworms
+    """
+    Open a thread to continuously attack monsters while loop_status is True.
+    """
+    while True:  # infinite loop to continuos attack monsters
         if loop_status:
             attack_next_monster()
    
-# creates a attack thread outside principal loop
+# creates an attack thread outside principal loop
 threadKillMonster = threading.Thread(target=thread_attack_monster)
 threadKillMonster.daemon = True  # defining thread as daemon to stop it when the principal program ends
 #threadKillMonster.start()
