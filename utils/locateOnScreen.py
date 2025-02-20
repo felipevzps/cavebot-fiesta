a0d53f90a2d151c3ac52e3da78e6313fcd4a30a0
felipevzps <felipe.vzps@gmail.com>
2025-02-20 12:15:53 -0300

    update trainer

--- utils/locateOnScreen.py
+++ utils/locateOnScreen.py
@@ -1,9 +1,13 @@
 import pyautogui
 from time import sleep
 
-#pyautogui.PAUSE = 0.01 # pyautogui delay default = 0.1
+REGION_HEALTH = (1747, 149, 116, 14)
+
+health_img = 'cavebot-fiesta/nostalther/images/health.PNG'
 
 # Function to locate the (left, top) position of each image
 while True:
+    health = pyautogui.locateOnScreen(health_img, confidence=0.8)  
     # Mouse position
     print(pyautogui.position())
+    print(health)
--- utils/trainer.py
+++ utils/trainer.py
@@ -5,39 +5,52 @@ from time import sleep
 import sys
 
 REGION_BATTLE = (1746, 362, 172, 164)
+REGION_HEALTH = (1747, 149, 116, 14)
+REGION_UH = (1592, 350)
+REGION_PLAYER = (1255, 246)
 REGION_MANA = (1840, 161, 24, 18)
-REGION_ARROW = (1592, 654)
+REGION_FOOD = (1592, 654)
 
 slimes_counter = -1
-loop_ativo = False  # Inittialy, the loop is off
+loop_status = False  # inittialy, the loop is off
 
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 
-def iniciar_loop():
-    global loop_ativo
-    loop_ativo = True
+def start_loop():
+    global loop_status
+    loop_status = True
 
-def pausar_loop():
-    global loop_ativo
-    loop_ativo = False
+def end_loop():
+    global loop_status
+    loop_status = False
 
-# Register keys to turn on/off the auto fishing
-keyboard.add_hotkey('page up', iniciar_loop)
-keyboard.add_hotkey('page down', pausar_loop)
+# register keys to turn on/off the trainer
+keyboard.add_hotkey('page up', start_loop)
+keyboard.add_hotkey('page down', end_loop)
 
-# Conjure rune (cast spell saved on F1 hotkey)
-def conjure_rune():
+# healing (use uh)
+def use_uh():
+  health = pyautogui.locateOnScreen('cavebot-fiesta/nostalther/images/health.PNG', confidence=0.8)
+  if health != None:
+    pyautogui.moveTo(REGION_UH)
+    pyautogui.click(REGION_UH, button='right')
+    pyautogui.moveTo(REGION_PLAYER)
+    pyautogui.click(REGION_PLAYER, button='left')
+    print(current_time, ':', 'Using one Ultimate Healing Rune...')
+
+# ml training (cast spell saved on F1 hotkey)
+def cast_spell():
   mana = pyautogui.locateOnScreen('cavebot-fiesta/nostalther/images/mana.PNG', confidence=0.8, region=REGION_MANA)
   if mana != None:
-    pyautogui.moveTo(REGION_ARROW)
-    pyautogui.click(REGION_ARROW, button='left')
+    pyautogui.moveTo(REGION_FOOD)
+    pyautogui.click(REGION_FOOD, button='left')
     keyboard.press_and_release('F1')
 
 def eat_food():
-   pyautogui.moveTo(REGION_ARROW)
-   for i in range(5):
-    pyautogui.click(REGION_ARROW, button='right')
+   pyautogui.moveTo(REGION_FOOD)
+   for i in range(3):
+     pyautogui.click(REGION_FOOD, button='right')
 
 def attack_next_slime():
   global slimes_counter
@@ -53,16 +66,17 @@ def attack_next_slime():
     pyautogui.moveTo(1754, 408)
     pyautogui.click(button="left")
     sleep(0.5)
-    eat_food() 
+    eat_food()
     slimes_counter += 1
     print(current_time, ':', 'Slimes killed: {}'.format(slimes_counter))
    
-print(current_time, ':', 'Starting trainer!')
+print(current_time, ':', 'PRESS PAGE UP TO START THE TRAINER')
 
 while True:
-  if loop_ativo:
+  if loop_status:
     attack_next_slime()
-    conjure_rune()
+    use_uh()
+    cast_spell()
     battle = pyautogui.locateOnScreen('cavebot-fiesta/nostalther/images/battle_name.PNG', confidence=0.9, region=(1744,361,65,17))
     if not battle:
        t = time.localtime()
